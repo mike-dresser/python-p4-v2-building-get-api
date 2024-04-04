964cf05831d5034088b7b982d0b65563d7610884
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-04 12:36:27 -0400

    completed except for association proxy

--- server/app.py
+++ server/app.py
@@ -20,7 +20,27 @@ def index():
     return "Index for Game/Review/User API"
 
 # start building your API here
+@app.route('/games')
+def games():
+    all_games = Game.query.all()
+    games = []
+    for game in all_games:
+        games.append(game.to_dict())
+    response = make_response(
+        jsonify(games),
+        200,
+        {"content-type": "application/json"}
+    )
+    return response
 
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game_obj = Game.query.filter_by(id=id).first()
+    if game_obj:
+        return make_response(game_obj.to_dict(), 200)
+    else:
+        return make_response({"error": f'No game with id {id} found'}, 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -14,9 +14,10 @@ metadata = MetaData(
 db = SQLAlchemy(metadata=metadata)
 
 
-class Game(db.Model):
+class Game(db.Model, SerializerMixin):
     __tablename__ = "games"
 
+    serialize_rules = ('-reviews.game',)
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True)
     genre = db.Column(db.String)
@@ -31,9 +32,10 @@ class Game(db.Model):
         return f"<Game {self.title} for {self.platform}>"
 
 
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = "reviews"
 
+    serialize_rules = ('-game.reviews', '-user.reviews')
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
     comment = db.Column(db.String)
@@ -50,9 +52,10 @@ class Review(db.Model):
         return f"<Review ({self.id}) of {self.game}: {self.score}/10>"
 
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
+    serialize_rules = ('-reviews.user',)
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
